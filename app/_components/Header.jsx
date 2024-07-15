cdeabd395eab07505830a48060413e02ca6a1c07
DMNihal65 <nihaldm65@gmail.com>
2024-07-15 15:50:25 +0530

    Added the navbar and the autentication for the home page

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,17 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import { SignInButton, UserButton, useUser, } from '@clerk/nextjs'
 import { ArrowLeftRight, ArrowRightCircle } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 function Header() {
+
+  const {user,isSignedIn}= useUser();
+
+
+  
   return (
     <header className='sticky top-0 z-50 bg-white shadow-md'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -27,17 +34,30 @@ function Header() {
               <li><Link href="/upgrade" className='text-gray-700 hover:text-white hover:scale-105 hover:bg-primary hover:rounded-lg hover:p-2  transition-all'>Upgrade</Link></li>
             </ul>
           </nav>
-          <div>
-            <Button className="bg-primary hover:bg-primary-dark transition">
-            <div className='flex items-center gap-2'>
-            <p>
-              Get Started
-              </p>
-              <ArrowRightCircle/>
-            </div>
-             
-            </Button>
+          {isSignedIn?
+          <Link href={'/dashboard'}>
+          <div className='flex gap-5 items-center justify-center'>
+            <Button varient="outline">Dashboard</Button>
+            <UserButton/>
           </div>
+          </Link> :
+          <SignInButton>
+             <div>
+          <Button className="bg-primary hover:bg-primary-dark transition">
+          <div className='flex items-center gap-2'>
+          <p>
+            Get Started
+            </p>
+            <ArrowRightCircle/>
+          </div>
+           
+          </Button>
+        </div>
+          </SignInButton>
+         
+
+        }
+          
         </div>
       </div>
     </header>
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 // const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en" data-theme="light">
       <body className={fontSans.className}>
       <Header/>
       {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
